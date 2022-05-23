e88dcff217e1e916728a5866173fdf064fb6a04b
Nazrul Islam Rakib <32672699+rakibpaucse@users.noreply.github.com>
2022-05-24 02:13:57 +0600

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,21 +1,8 @@
-// import axios from 'axios';
-// const Axios = axios.create({
-//   baseURL: 'https://as.mutualempressa.com/lekapora/public/api',
-//   timeout: 1000,
-//   headers: {
-//     'X-Custom-Header': 'foobar',
-//     'Content-Type': 'text/plain',
-//   },
-// });
-// // Axios.defaults.headers.post['Content-Type'] =
-// //   'application/x-www-form-urlencoded';
 
-// export default Axios;
 
 import axios from 'axios';
-// const baseDomain = 'https://as.mutualempressa.com/lekapora/public/api/'; // API for products
-// const baseDomain = 'http://localhost:8081/'; // API for products
-const baseDomain = 'http://links.shaheenagrofood.com/'; // API for products 
+
+const baseDomain = 'http://links.shaheenagrofood.com/'; // API  
 
 export const customHeaders = {
   Accept: 'application/json',
